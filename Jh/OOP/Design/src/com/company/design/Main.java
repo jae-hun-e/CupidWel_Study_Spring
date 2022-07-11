9ac986ec1f6b75a6cddc4012bed8695e1a51cc82
jae-hun-e <wognskec@hanyang.ac.kr>
2022-07-11 10:49:55 +0900

    facde 패턴

--- Jh/OOP/Design/src/com/company/design/Main.java
+++ Jh/OOP/Design/src/com/company/design/Main.java
@@ -3,6 +3,10 @@ package com.company.design;
 import com.company.design.adapter.*;
 import com.company.design.aop.AopBrowser;
 import com.company.design.decorator.*;
+import com.company.design.facade.Ftp;
+import com.company.design.facade.Reader;
+import com.company.design.facade.SftpClient;
+import com.company.design.facade.Writer;
 import com.company.design.observer.Button;
 import com.company.design.observer.IButtonListener;
 import com.company.design.proxy.Browser;
@@ -108,6 +112,7 @@ AtomicLong start = new AtomicLong();
          */
 
 //        observer 예제
+        /*
         Button button = new Button("버튼");
 
         // 익명 class로 전달
@@ -121,6 +126,33 @@ AtomicLong start = new AtomicLong();
         button.click("메세지 전달 : click2");
         button.click("메세지 전달 : click3");
         button.click("메세지 전달 : click4");
+         */
+
+//        Facade 예제
+        //기존 코드
+        Ftp ftpClient = new Ftp("www.foo.co.kr", 22, "/home/etc");
+        ftpClient.connect();
+        ftpClient.moveDirectory();
+
+        Writer writer = new Writer("text.tmp");
+        writer.fileConnect();
+        writer.write();
+
+        Reader reader = new Reader("text.tmp");
+        reader.fileConnect();
+        reader.fileRead();
+
+        reader.fileDisconnect();
+        writer.fileDisconnect();
+        ftpClient.disConnect();
+
+        // facade 코드
+        SftpClient sftpClient = new SftpClient("www.foo.co.kr", 22, "/home/etc", "text.tmp");
+
+        sftpClient.connect();
+        sftpClient.write();
+        sftpClient.read();
+        sftpClient.disConnect();
 
     }
 
